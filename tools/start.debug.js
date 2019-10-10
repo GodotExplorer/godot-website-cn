b50e24066392bee0ebddbff07917bc1a25e7c2a4
geequlim <geequlim@gmail.com>
2019-10-10 21:24:59 +0800

    添加VSCode调试配置

--- tools/start.debug.js
+++ tools/start.debug.js
@@ -1,4 +1,4 @@
-const shelljs = require('shelljs');
-shelljs.exec("yarn webpack --config webpack.config.js")
+const shelljs = require("shelljs");
+shelljs.exec("yarn webpack --config webpack.config.js");
 shelljs.cd("dist");
 shelljs.exec("node server.js --debug");
--- tools/start.release.js
+++ tools/start.release.js
@@ -1,4 +1,4 @@
-const shelljs = require('shelljs');
-shelljs.exec("yarn webpack --config webpack.config.js --env.production")
+const shelljs = require("shelljs");
+shelljs.exec("yarn webpack --config webpack.config.js --env.production");
 shelljs.cd("dist");
 shelljs.exec("node server.js --release");
