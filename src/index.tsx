191b10b52838af0797b78d8b77b9693301b4cfa1
logoss <463010136@qq.com>
2019-10-19 21:02:39 +0800

    header and footer 2

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,8 @@ import { Router, Route, Switch} from 'react-router-dom';
 import { createBrowserHistory } from 'history';
 import Home from './views/pages/Home';
 import NotFound from './views/pages/NotFound';
+import Test from 'views/pages/Test';
+
 
 const history = createBrowserHistory();
 
@@ -11,6 +13,7 @@ ReactDOM.render(
 	<Router history={history}>
 		<Switch>
 			<Route exact path="/" component={Home} />
+			<Route exact path="/test" component={Test} />
 			<Route path="*" component={NotFound}/>
 		</Switch>
 	</Router>,
---
+++ src/views/pages/Test/index.tsx
@@ -0,0 +1,14 @@
+import * as React from "react"
+import Header from "views/components/Header"
+import Footer from "views/components/Footer"
+
+export default class Test extends React.Component{
+    render(){
+        return(
+            <div>
+                <Header></Header>
+                <Footer></Footer>
+            </div>
+        )
+    }
+}
